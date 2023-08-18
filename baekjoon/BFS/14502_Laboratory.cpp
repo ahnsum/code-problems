4570dc50fca73d5493946898abf2da32332a75af
ahnsum <113354933+ahnsum@users.noreply.github.com>
2023-08-18 17:31:51 +0900

    Update 14502_Laboratory.cpp

--- baekjoon/BFS/14502_Laboratory.cpp
+++ baekjoon/BFS/14502_Laboratory.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 queue<pair<int, int>> q;
 
-int map[MAX][MAX];
-int tmp[MAX][MAX];
-int visit[MAX][MAX] = { 0 };
+int map[MAX][MAX]; // 입력
+int tmp[MAX][MAX]; // 복사
+int arr[MAX][MAX]; // 조합
 
 vector<pair<int, int>> virus, space;
 
@@ -20,18 +20,18 @@ int dy[4] = { 0,1,0,-1 };
 int N, M;
 int result = 0;
 
-void copyMap() // 맵 복사
+void copyMap(int copyArr[MAX][MAX], int newArr[MAX][MAX]) // 맵 복사
 {
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < M; j++)
 		{
-			tmp[i][j] = map[i][j];
+			copyArr[i][j] = newArr[i][j];
 		}
 	}
 }
 
-void countMaxSpace() // 빈 칸 영역의 최대 크기
+void countMaxSpace() // 안전 영역의 최대 크기
 {
 	int cnt = 0;
 
@@ -39,9 +39,7 @@ void countMaxSpace() // 빈 칸 영역의 최대 크기
 	{
 		for (int j = 0; j < M; j++)
 		{
-			visit[i][j] = 0;
-
-			if (tmp[i][j] == 0)
+			if (arr[i][j] == 0)
 			{
 				cnt++;
 			}
@@ -51,10 +49,10 @@ void countMaxSpace() // 빈 칸 영역의 최대 크기
 	result = max(result, cnt);
 }
 
-void bfs(int n, int m)
+void bfs()
 {
-	q.push({ n, m });
-	
+	copyMap(arr, tmp);
+
 	while (!q.empty())
 	{
 		int x = q.front().first;
@@ -67,13 +65,12 @@ void bfs(int n, int m)
 			int nx = x + dx[i];
 			int ny = y + dy[i];
 
+			if (arr[nx][ny]) continue;
 			if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue; // 맵의 범위 안에 있는지 체크
 
-			if (tmp[nx][ny] == 0) // 빈 칸이면 바이러스 감염
-			{
-				tmp[nx][ny] = 2;
-				q.push({ nx, ny });
-			}
+			// 빈 칸이면 바이러스 감염
+			arr[nx][ny] = 2;
+			q.push({ nx, ny });
 		}
 	}
 }
@@ -82,10 +79,10 @@ void spreadVirus() // 바이러스 퍼뜨리기
 {
 	for (int i = 0; i < virus.size(); i++)
 	{
-		bfs(virus[i].first, virus[i].second);
+		q.push({ virus[i].first, virus[i].second });
 	}
 
-	countMaxSpace();
+	bfs();
 }
 
 void wall(int cnt) // 벽 세우기
@@ -93,36 +90,37 @@ void wall(int cnt) // 벽 세우기
 	if (cnt == 3)
 	{
 		spreadVirus();
+		countMaxSpace();
 
 		return;
 	}
 
-	for (int i = 0; i < space.size(); i++)
+	for (int i = 0; i < space.size(); i++) // 빈 칸을 벽 하나로 선택하고, 2개의 벽을 조합해나가는 방식
 	{
 		int x = space[i].first;
 		int y = space[i].second;
 
-		tmp[x][y] = 1;
-		wall(cnt + 1);
-		tmp[x][y] = 0;
+		if (tmp[x][y] == 0)
+		{
+			tmp[x][y] = 1;
+			wall(cnt + 1);
+			tmp[x][y] = 0;
+		}
 	}
 }
 
 void wallMaker()
 {
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < space.size(); i++)
 	{
-		for (int j = 0; j < M; j++)
-		{
-			if (map[i][j] == 0)
-			{
-				copyMap(); // 맵 초기화
-				
-				tmp[i][j] = 1;
-				wall(1);
-				tmp[i][j] = 0;
-			}
-		}
+		int x = space[i].first;
+		int y = space[i].second;
+
+		copyMap(tmp, map); // 맵 복사(초기화)
+
+		tmp[x][y] = 1; // 빈 칸 하나를 벽으로 지정
+		wall(1);
+		tmp[x][y] = 0;
 	}
 }
 
