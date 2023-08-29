bbfc14f80c6b203c4207c76794e9ad3d2930f43e
ahnsum <113354933+ahnsum@users.noreply.github.com>
2023-08-29 14:53:01 +0900

    Update TargetNumber.cpp

--- programmers/HighScoreKit/DFS/TargetNumber.cpp
+++ programmers/HighScoreKit/DFS/TargetNumber.cpp
@@ -3,27 +3,39 @@
 
 using namespace std;
 
-void dfs(vector<int> numbers, int target, int index, int result, int& answer)
+void dfs(vector<int> numbers, int idx, int result, int target, int& answer)
 {
-    if (index == numbers.size())
-    {
-        if (result == target)
-        {
-            answer++;
-        }
-        
-        return;
-    }
-    
-    dfs(numbers, target, index + 1, result + numbers[index], answer);
-    dfs(numbers, target, index + 1, result - numbers[index], answer);
+	if (idx == numbers.size()) // numbers를 전부 돌았을 때
+	{
+		if (result == target) // target과 같다면
+		{
+			answer++;
+		}
+		return;
+	}
+	
+	dfs(numbers, idx + 1, result + numbers[idx], target, answer);
+	dfs(numbers, idx + 1, result - numbers[idx], target, answer);
 }
 
-int solution(vector<int> numbers, int target) 
+int solution(vector<int> numbers, int target)
 {
-    int answer = 0;
-    
-    dfs(numbers, target, 0, 0, answer);
-    
-    return answer;
+	int answer = 0;
+
+	dfs(numbers, 0, 0, target, answer);
+
+	return answer;
+}
+
+int main()
+{
+	vector<int> numbers = { 1, 1, 1, 1, 1 };
+	//vector<int> numbers = { 4, 1, 2, 1 };
+
+	int target = 3; 
+	//int target = 4;
+
+	cout << solution(numbers, target) << endl;
+
+	return 0;
 }
