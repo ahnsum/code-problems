2b6802320feca289a37f4f0d1bd6a334c5831158
ahnsum <113354933+ahnsum@users.noreply.github.com>
2023-08-29 16:04:08 +0900

    Update GameMapShortDist.cpp

--- programmers/HighScoreKit/BFS/GameMapShortDist.cpp
+++ programmers/HighScoreKit/BFS/GameMapShortDist.cpp
@@ -1,59 +1,68 @@
-#include <vector>
+#include <iostream>
 #include <queue>
-
-#define MAX 100
+#include <vector>
 
 using namespace std;
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+
+queue<pair<pair<int, int>, int>> q;
+bool visited[100][100] = { 0 };
 
 int bfs(vector<vector<int>> maps)
 {
-    queue<pair<pair<int,int>, int>> q;
-    bool visited[MAX][MAX] = {0};
+	int N = maps.size();
+	int M = maps[0].size();
 
-    int N = maps.size();
-    int M = maps[0].size();
-    
-    visited[0][0] = true;
-    q.push({{0,0}, 1});
-    
-    while(!q.empty())
-    {
-        int x = q.front().first.first;
-        int y = q.front().first.second;
-        int cnt = q.front().second;
-        
-        q.pop();
-        
-        if (x == N-1 && y == M-1)
-        {
-            return cnt;
-        }
-        
-        for (int i = 0; i < 4; i++)
-        {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+	q.push({ {0, 0}, 1 });
+	visited[0][0] = true;
 
-            if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-            if (maps[nx][ny] == 0) continue;
-            if (visited[nx][ny]) continue;
+	while (!q.empty())
+	{
+		int x = q.front().first.first;
+		int y = q.front().first.second;
+		int cnt = q.front().second;
 
-            visited[nx][ny] = true;
-            q.push({{nx, ny}, cnt + 1});
-        }
-    }
-    
-    return -1;
+		q.pop();
+
+		if (x == N - 1 && y == M - 1)
+		{
+			return cnt;
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+			if (visited[nx][ny]) continue;
+			if (maps[nx][ny] == 0) continue;
+
+			visited[nx][ny] = true;
+			q.push({{ nx, ny }, cnt + 1});
+		}
+	}
+
+	return -1;
 }
 
 int solution(vector<vector<int>> maps)
 {
-    int answer = 0;
-    
-    answer = bfs(maps);
-    
-    return answer;
+	int answer = 0;
+
+	answer = bfs(maps);
+
+	return answer;
+}
+
+int main()
+{
+	vector<vector<int>> maps = { {1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1} };
+	//vector<vector<int>> maps = { {1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 0}, {0, 0, 0, 0, 1} };
+	
+	cout << solution(maps) << endl;
+
+	return 0;
 }
